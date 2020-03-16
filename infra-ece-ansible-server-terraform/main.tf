8e8b2121836c9bf17670b98a28fdfa3a6c39b6b3
silveira <silveira@silveira.pc>
2020-03-16 12:12:00 -0300

    ansible server

---
+++ infra-ece-ansible-server-terraform/main.tf
@@ -0,0 +1,171 @@
+// Configure the Google Cloud provider
+provider "google" {
+ credentials = file("credentials.json")
+ project     = "inlaid-lane-270316"
+ region      = "us-east4"
+}
+
+resource "google_compute_disk" "ece-01" {
+ name = "ece-01-data"
+ type = "pd-ssd"
+ zone = "us-east4-a"
+ size = "50"
+}
+
+resource "google_compute_disk" "ece-02" {
+ name = "ece-02-data"
+ type = "pd-ssd"
+ zone = "us-east4-b"
+ size = "50"
+}
+
+resource "google_compute_disk" "ece-03" {
+ name = "ece-03-data"
+ type = "pd-ssd"
+ zone = "us-east4-c"
+ size = "50"
+}
+
+// A single Google Cloud Engine instance
+resource "google_compute_instance" "ece-01" {
+ name         = "ece-01"
+ machine_type = "n1-standard-8"
+ zone         = "us-east4-a"
+ hostname     = "ece-01.srv"
+
+ boot_disk {
+   initialize_params {
+     image = "centos-cloud/centos-7"
+     size  = "30"
+   }
+ }
+
+ attached_disk {
+   source      = "ece-01-data"
+   device_name = "sdb"
+ }
+
+ network_interface {
+   network    = "default"
+   network_ip = "10.150.0.10"
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+ metadata = {
+   ssh-keys = "silveira:${file("~/.ssh/silveira.pub")}"
+ }
+}
+
+resource "google_compute_instance" "ece-02" {
+ name         = "ece-02"
+ machine_type = "n1-standard-8"
+ zone         = "us-east4-b"
+ hostname     = "ece-02.srv"
+
+ boot_disk {
+   initialize_params {
+     image = "centos-cloud/centos-7"
+     size  = "30"
+   }
+ }
+
+ attached_disk {
+   source      = "ece-02-data"
+   device_name = "sdb"
+ }
+
+ network_interface {
+   network    = "default"
+   network_ip = "10.150.0.11"
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+ metadata = {
+   ssh-keys = "silveira:${file("~/.ssh/silveira.pub")}"
+ }
+}
+
+resource "google_compute_instance" "ece-03" {
+ name         = "ece-03"
+ machine_type = "n1-standard-8"
+ zone         = "us-east4-c"
+ hostname     = "ece-03.srv"
+
+ boot_disk {
+   initialize_params {
+     image = "centos-cloud/centos-7"
+     size  = "30"
+   }
+ }
+
+ attached_disk {
+   source      = "ece-03-data"
+   device_name = "sdb"
+ }
+
+ network_interface {
+   network    = "default"
+   network_ip = "10.150.0.12"
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+ metadata = {
+   ssh-keys = "silveira:${file("~/.ssh/silveira.pub")}"
+ }
+
+ provisioner "local-exec" {
+   command = "ansible-playbook -i /home/silveira/ece-ansible/hosts /home/silveira/ece-ansible/playbooks/ece.yml"
+ }
+}
+
+resource "google_compute_target_pool" "ece-backend" {
+  name = "ece-backend"
+
+  instances = [
+    google_compute_instance.ece-01.self_link,
+    google_compute_instance.ece-02.self_link,
+    google_compute_instance.ece-03.self_link,
+  ]
+  region = "us-east4"
+}
+
+resource "google_compute_forwarding_rule" "lb" {
+ name                  = "lb-ece"
+ region                = "us-east4"
+ load_balancing_scheme = "EXTERNAL"
+ ip_protocol           = "TCP"
+ port_range            = "9243-12443"
+ target                = google_compute_target_pool.ece-backend.self_link
+}
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+/*
+resource "google_compute_instance_group" "ece-01" {
+ name      = "ece-01"
+ zone      = "us-east4-a"
+ instances = [
+   google_compute_instance.ece-01.self_link
+ ]
+}
+
+resource "google_compute_instance_group" "ece-02" {
+ name      = "ece-02"
+ zone      = "us-east4-b"
+ instances = [
+   google_compute_instance.ece-02.self_link
+ ]
+}
+
+resource "google_compute_instance_group" "ece-03" {
+ name      = "ece-03"
+ zone      = "us-east4-c"
+ instances = [
+   google_compute_instance.ece-03.self_link
+ ]
+}
+*/
+
