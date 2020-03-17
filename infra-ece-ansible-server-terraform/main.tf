d510a7b2821746f36aaefaf84bbb04950ab7efb5
silveira <silveira@silveira.pc>
2020-03-17 20:43:22 -0300

    local-exec

--- infra-ece-ansible-server-terraform/main.tf
+++ infra-ece-ansible-server-terraform/main.tf
@@ -137,6 +137,17 @@ resource "google_compute_forwarding_rule" "lb" {
  target                = google_compute_target_pool.ece-backend.self_link
 }
 
+resource "null_resource" "hosts" {
+ triggers = {
+   anything1 = google_compute_instance.ece-01.network_interface.0.access_config.0.nat_ip
+   anything2 = google_compute_instance.ece-02.network_interface.0.access_config.0.nat_ip
+   anything3 = google_compute_instance.ece-03.network_interface.0.access_config.0.nat_ip
+ }
+ provisioner "local-exec" {
+   command = "cd /home/silveira/ece-ansible; ansible-playbook -i hosts playbooks/ece.yml"
+ }
+}
+
 //-------------------------------------------------------------------------------------------------------------------------
 
 /*
